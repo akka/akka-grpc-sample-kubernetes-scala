f00c617b02fcc6fe9ddc987b6e726f0f81768bad
Arnout Engelen <arnouten@bzzt.net>
2018-11-19 12:00:30 +0100

    Install rp as documented
    at https://developer.lightbend.com/docs/lightbend-orchestration/current/setup/cli-installation.html#install-the-cli

--- scripts/setup-rp.sh
+++ scripts/setup-rp.sh
@@ -2,9 +2,12 @@
 
 set -exu
 
-RP_VERSION=1.3.1
-DEB=reactive-cli_$VERSION-xenial_amd64.deb
+# See https://developer.lightbend.com/docs/lightbend-orchestration/current/setup/cli-installation.html#install-the-cli for instructions for your platform
 
-wget https://dl.bintray.com/lightbend/deb/$DEB
-
-sudo dpkg -i $DEB
+wget -qO - https://downloads.lightbend.com/rp/keys/bintray-debian | \
+    sudo apt-key add - && \
+    echo "deb https://dl.bintray.com/lightbend/deb $(lsb_release -cs) main" | \
+    sudo tee /etc/apt/sources.list.d/lightbend.list && \
+    sudo apt-get install apt-transport-https -y && \
+    sudo apt-get update && \
+    sudo apt-get install reactive-cli
